fec14f08b8a81f0db21d98dd77a3785486ae1da4
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-07-19 12:43:00 -0400

    wrote pizza-routes file and created the routes/api dir

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,6 +1,7 @@
 const { Pizza } = require('../models');
 
 const pizzaController = {
+  //findAll Pizzas
   getAllPizza(req, res) {
     Pizza.find({})
       .then((dbPizzaData) => {
@@ -11,6 +12,7 @@ const pizzaController = {
         res.status(400).json(err);
       });
   },
+  //findById a certain pizza
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
       .then((dbPizzaData) => {
@@ -25,6 +27,37 @@ const pizzaController = {
         console.log(err);
         res.status(400).json(err);
       });
+  },
+  //create a new pizza
+  createPizza: function () {
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.status(400).json(err));
+  },
+  //update a pizza by id
+  updatePizza: function ({ params, body }, res) {
+    PizzafindOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbPizzaData) => {
+        //if ID not found return 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  // delete pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
   },
 };
 
